fa7f673415cbf381bd02569804a7134273447849
SLBarnes23 <150044472+SLBarnes23@users.noreply.github.com>
2024-06-19 16:21:34 -0230

    added init features

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
 global.DEBUG = true;
 
 const fs = require('fs');
+const path = require('path');
+const { initializeApplication } = require('./init.js');
 
-const {initializeApplication} = require('./init.js')
-const {configApplication} = require('./config.js')
-const {tokenApplication} = require('./token.js')
+// Load the configuration JSON
+const configPath = path.join(__dirname, 'config.json');
+let config = {};
+
+try {
+    config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+} catch (error) {
+    console.error('Error loading configuration:', error);
+}
 
 const myArgs = process.argv.slice(2); // Skip the first two arguments
 
 if (DEBUG) {
     if (myArgs.length >= 1) console.log('myArgs: ', myArgs);
+    console.log('Config: ', config);
 }
 
 switch (myArgs[0]) {
     case 'init':
     case 'i':
         if (DEBUG) console.log(myArgs[0], '- init the app.');
-        initializeApplication();
+        initializeApplication(config); // Pass config to the initializer
         break;
-
     case 'config':
     case 'c':
         if (DEBUG) console.log(myArgs[0], '- create the config folders and files.');
-        configApplication();
+        // Add your config logic here
         break;
     case 'token':
     case 't':
         if (DEBUG) console.log(myArgs[0], '- manage the tokens.');
-        tokenApplication();
+        // Add your token management logic here
         break;
-        
     case '--help':
     case '--h':
     default:
         if (DEBUG) console.log(myArgs[0], '- display help');
         fs.readFile(__dirname + "/usage.txt", (error, data) => {
-            if(error) throw error;
+            if (error) throw error;
             console.log(data.toString());
         });
         break;
 }
-
-
-
-     
--- init.js
+++ init.js
@@ -1,36 +1,99 @@
 const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const myArgs = process.argv.slice(2);
 
-function initializeApplication() {
-    if(DEBUG) console.log('initializeApplication()');
+const folders = ['models', 'views', 'routes', 'logs', 'json'];
 
-    switch (myArgs[1]) {
-    case '--all':
-        if(DEBUG) console.log('--all createFolders() & createFiles()');
-        createFolders();
-        createFiles();
-        break;
-    case '--cat':
-        if(DEBUG) console.log('--cat createFiles()');
-        createFiles();
-        break;
-    case '--mk':
-        if(DEBUG) console.log('--mk createFolders()');
-        createFolders();
-        break;
-    case '--help':
-    case '--h':
-    default:
-        fs.readFile(__dirname + "/usage.txt", (error, data) => {
-            if (error) throw error;              
-            console.log(data.toString());
-        });
+const configjson ={
+    name: 'AppConfigCLI',
+    version: '1.0.0',
+    description: 'The Command Line Interface (CLI) for the MyApp.',
+    main: 'myapp.js',
+    superuser: 'admin',
+    database: 'exampledb'
+};
+
+function createFiles() {
+    if(DEBUG) console.log('init.createFiles()');
+    try {
+        let configdata = JSON.stringify(configjson, null, 2);
+        let fileName = './json/config.json'
+        if(!fs.existsSync(path.join(__dirname, fileName))) {
+            fs.writeFile(fileName, configdata, (err) => {
+                if(err) {
+                    if (err.code == 'ENOENT') {
+                        myEventEmitter.emit('event', fileName, 'ERROR', `The ${fileName} was in error, no file or directory.`);
+                        console.log('No file or directory, has the directory been created.');
+                    }
+                    else
+                        console.log(err);
+                }
+                else {
+                    myEventEmitter.emit('event', fileName, 'INFO', `The ${fileName} was successfully written to disk.`);
+                    console.log('Data written to config file.');
+                }
+            })
+        } else {
+          myEventEmitter.emit('event', fileName, 'INFO', `The ${fileName} already exists.`);
+          console.log('config file already exists.');
+        }
+    } catch(err) {
+        if (err.code == 'ENOENT')
+            console.log('no file or directory');
+        else
+            console.log(err);
+    }
+  };
+
+
+async function createFolders() {
+    if (DEBUG) console.log('init.createFolders()');
+    let mkcount = 0;
+    for (const folder of folders) {
+        if (DEBUG) console.log(folder);
+        try {
+            if (!fs.existsSync(path.join(__dirname, folder))) {
+                await fsPromises.mkdir(path.join(__dirname, folder));
+                mkcount++;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    if (mkcount === 0) {
+        console.log('All folders already exist.');
+    } else if (mkcount <= folders.length) {
+        console.log(mkcount + ' of ' + folders.length + ' folders were created.');
+    } else {
+        console.log('All folders successfully created.');
     }
 }
 
-function createFolders() {
-    // Your logic to create folders
+function initializeApplication() {
+    if (DEBUG) console.log('initializeApplication()');
+
+    switch (myArgs[1]) {
+        case '--all':
+            if (DEBUG) console.log('--all createFolders() & createFiles()');
+                        break;
+        case '--cat':
+            if (DEBUG) console.log('--cat createFiles()');
+            createFiles();
+            break;
+        case '--mk':
+            if (DEBUG) console.log('--mk createFolders()');
+            createFolders();
+            break;
+        case '--help':
+        case '--h':
+        default:
+            fs.readFile(__dirname + "/usage.txt", (error, data) => {
+                if (error) throw error;
+                console.log(data.toString());
+            });
+    }
 }
 
 function createFiles() {
